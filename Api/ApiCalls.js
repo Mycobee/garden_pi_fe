39064daab874e009b86a62c8f51b769cec03bf13
David Engel <Davidjengel2012@gmail.com>
2019-09-02 14:08:43 -0600

    Change typo in apicalls

--- Api/ApiCalls.js
+++ Api/ApiCalls.js
@@ -1,7 +1,7 @@
-import { apiKey } from './ApiKey'
+import { ApiKey } from './ApiKey'
 
 export const fetchWeather = () => {
-  return fetch(`https://api.darksky.net/forecast/${apiKey}/39.751048,-104.996659`)
+  return fetch(`https://api.darksky.net/forecast/${ApiKey}/39.751048,-104.996659`)
   .then(res => res.json())
   .then(res => res)
   .catch(error => error.message)
