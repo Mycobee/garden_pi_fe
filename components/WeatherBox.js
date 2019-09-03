565142e9d6f43c241116d6b954ebc10581c7e1ab
David Engel <Davidjengel2012@gmail.com>
2019-09-02 22:21:29 -0600

    Add views to weatherbox for formatting

--- components/WeatherBox.js
+++ components/WeatherBox.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
-const WeatherBox = ({ summary, highTemp, lowTemp, time }) => (
+const WeatherBox = ({ summary, highTemp, lowTemp, time, icon }) => (
   <View style={styles.widget}>
-    <Text>{time}</Text>
-    <Text>High: {highTemp}</Text>
-    <Text>Low: {lowTemp}</Text>
-    <Text>{summary}</Text>
+    <View style={styles.textContainer}>
+      <Text>{time}</Text>
+      <Text>High: {highTemp}</Text>
+      <Text>Low: {lowTemp}</Text>
+      <Text style={styles.summary}>{summary}</Text>
+    </View>
+    <View style={styles.icon}>
+      {icon}
+    </View>
   </View>
 )
 
 const styles = StyleSheet.create({
   widget: {
-    padding: 5,
-    justifyContent: 'center',
-    margin: 10
+    padding: 0,
+  },
+  summary: {
+    width: 125,
+    marginTop: 5
+  },
+  textContainer: {
+    height: 115
+  },
+  icon: {
+    width: 70,
+    height: 70,
   }
 })
 
