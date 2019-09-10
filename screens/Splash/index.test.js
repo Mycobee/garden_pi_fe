d7a88579c39cf83e2dff0a3b1beb13f945783c7b
Colby Allen <colbyallen012@gmail.com>
2019-09-09 22:11:32 -0600

    add test for fetchWeather call

--- screens/Splash/index.test.js
+++ screens/Splash/index.test.js
@@ -2,9 +2,17 @@ import React from 'react';
 import Splash from './index';
 import 'react-native';
 import renderer from 'react-test-renderer';
+import * as constants from '../../Api/ApiCalls'
 
 
 test('renders correctly', () => {
   const tree = renderer.create(<Splash />).toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test('getWeather should fetch current weather', async () => {
+  constants.fetchWeather = jest.fn()
+  constants.fetchWeather()
+  expect(constants.fetchWeather).toHaveBeenCalled()
+})
+
