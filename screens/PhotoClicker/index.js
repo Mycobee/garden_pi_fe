d57ba988bdecfceb3a78cb38ea98187c2b519d75
David Engel <Davidjengel2012@gmail.com>
2019-09-10 22:15:55 -0600

    Add snap photo button and function that provides URI when photo is taken

--- screens/PhotoClicker/index.js
+++ screens/PhotoClicker/index.js
@@ -12,7 +12,14 @@ export default class PhotoClicker extends React.Component {
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === 'granted' });
-  }
+  };
+
+  snapPhoto = async () => {
+    if (this.camera) {
+      let photo = await this.camera.takePictureAsync();
+      console.log(photo)
+    }
+  };
 
   render() {
     const { hasCameraPermission } = this.state;
@@ -23,7 +30,12 @@ export default class PhotoClicker extends React.Component {
     } else {
       return (
         <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
+          <Camera 
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style={{ flex: 1 }} type={this.state.type}
+          >
             <View
               style={{
                 flex: 1,
@@ -47,7 +59,35 @@ export default class PhotoClicker extends React.Component {
                         : Camera.Constants.Type.back,
                   });
                 }}>
-                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+                <Text 
+                  style={{ 
+                    fontSize: 18, 
+                    marginBottom: 10, 
+                    color: 'white' 
+                  }}
+                > 
+                  Flip 
+                </Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                  marginLeft: 'auto',
+                  marginRight: 'auto',
+                  marginBottom: 25
+                }}
+                onPress={this.snapPhoto}>
+                <Text 
+                  style={{ 
+                    fontSize: 18, 
+                    marginBottom: 10, 
+                    color: 'white' 
+                  }}
+                > 
+                  Snap 
+                </Text>
               </TouchableOpacity>
             </View>
           </Camera>
