6d2f59a6cd1f1fdfc10295659c30c7659a986bfb
David Engel <Davidjengel2012@gmail.com>
2019-10-01 22:11:49 -0600

    Clean code in utilities

--- utilities/TimeCalculator.js
+++ utilities/TimeCalculator.js
@@ -3,9 +3,9 @@ export const getRecordingTime = complexTime => {
   const hourRecorded = date.getHours();
   const minuteRecorded = date.getMinutes().toString();
   const formatMinute = () => {
-    return minuteRecorded.length == 1 
-      ? `0${date.getMinutes()}` 
-      : `${date.getMinutes()}`
+    return minuteRecorded.length == 1 ?
+    `0${date.getMinutes()}` :
+    `${date.getMinutes()}`
   }
   const finalTimeRecorded = () => {
     if (hourRecorded >= 12) {
